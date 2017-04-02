495fb382c49a54459fa837a5c8dd05839f1b7088
Dinesh <dinesh@Dineshs-MacBook-Air.local>
2017-04-02 13:53:34 +0530

    Dinesh | Schema created, fetched and json construction completed

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ var express = require('express');
 var repository = require('./repository')
 var server = express();
 
-server.listen(8482, function() {
+server.listen(8482, function () {
 	console.log("NodeJs server started successfully in port : 8482");
 });
 
-server.get('/', function(req, res) {
-	res.send(repository.printStudent());
+server.get('/', function (req, res) {
+	var callback = function (err, json) {
+		if (err) throw err;
+		res.send(json);
+	};
+	repository.returnSingleStudent(callback);
 });
 
-server.get('/parents', function(req, res) {
+server.get('/parents', function (req, res) {
 	res.send('You have reached Parents Page');
 });
 
-server.get('/students', function(req, res) {
-	res.send('You have reached Students Page');
+server.get('/students', function (req, res) {
+	// res.send(repository.findAll());
 });
--- repository.js
+++ repository.js
@@ -1,4 +1,3 @@
-
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
@@ -6,27 +5,48 @@ var Schema = mongoose.Schema;
 mongoose.connect('mongodb://localhost:27017/nodejsdb');
 
 var studentSchema = new Schema({
-	name : { type: String, required: true},
-	rollNo : { type : Number, required: true, unique : true},
-	className : {type : String},
-	createdAt : Date
+  name: {
+    type: String,
+    required: true
+  },
+  rollNo: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  className: {
+    type: String
+  },
+  createdAt: Date
 });
 
 var Student = mongoose.model('Student', studentSchema);
 
-
-function printStudent() {
-  var newStudent =  Student({
-    name : 'Dinesh',
-    rollNo : 1,
-    className : 'Ece A',
+function returnSingleStudent(callback) {
+  Student.count({
+    name: 'Dinesh'
+  }, function (err, c) {
+    if (err) throw callback(err, null);
+    if (c == 1) {
+      Student.find().lean().exec(function (err, students) {
+        callback(null, JSON.stringify(students));
+      });
+    } else {
+      var newStudent = Student({
+        name: 'Dinesh',
+        rollNo: 1,
+        className: 'Ece A',
+      });
+      newStudent.save(function (err) {
+        if (err) throw callback(err, null);
+        Student.find().lean().exec(function (err, students) {
+          return callback(null, JSON.stringify(students));
+        });
+      });
+    }
   });
-  newStudent.save(function(err){
-    if(err) throw err;
-    console.log('user created !');
-  });
-  return "Repository js working !";
-};
+}
 
-module.exports.printStudent = printStudent;
+module.exports.printStudent = returnSingleStudent;
+// module.exports.findAll = findAll;
 module.exports.Student = Student;
